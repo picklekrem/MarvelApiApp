3cecad2342079256630d1cac5235c5e8da61ad83
Ekrem <ekrem@3pounds.io>
2022-01-03 15:29:12 +0300

    First commit, good commit :)

---
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Ekrem Özkaraca on 3.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
